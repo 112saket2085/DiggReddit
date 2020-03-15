e7868b32f669be6430e63f019e145cc04aa10419
user <jha.saket350@gmail.com>
2020-03-15 10:04:46 +0530

    TopicListViewModel - Added TopicList ViewModel

---
+++ app/src/main/java/com/example/diggreddit/viewmodel/TopicListViewModel.java
@@ -0,0 +1,13 @@
+package com.example.diggreddit.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class TopicListViewModel extends AndroidViewModel {
+
+    public TopicListViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
